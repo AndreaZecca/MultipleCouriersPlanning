044a5667fa0d74ebf8e7e22e1abd9641b85ade0a
Stefano <juststep.2000@gmail.com>
2023-04-18 17:33:25 +0000

    Bug fixes and added new files for SAT

---
+++ SAT/utils.py
@@ -0,0 +1,26 @@
+from z3 import *
+
+
+###################################################################################################### K- sequential encodings
+
+def at_least_k_seq(bool_vars, k, name):
+    return at_most_k_seq([Not(var) for var in bool_vars], len(bool_vars)-k, name)
+
+def at_most_k_seq(bool_vars, k, name):
+    constraints = []
+    n = len(bool_vars)
+    s = [[Bool(f"s_{name}_{i}_{j}") for j in range(k)] for i in range(n - 1)]
+    constraints.append(Or(Not(bool_vars[0]), s[0][0]))
+    constraints += [Not(s[0][j]) for j in range(1, k)]
+    for i in range(1, n-1):
+        constraints.append(Or(Not(bool_vars[i]), s[i][0]))
+        constraints.append(Or(Not(s[i-1][0]), s[i][0]))
+        constraints.append(Or(Not(bool_vars[i]), Not(s[i-1][k-1])))
+        for j in range(1, k):
+            constraints.append(Or(Not(bool_vars[i]), Not(s[i-1][j-1]), s[i][j]))
+            constraints.append(Or(Not(s[i-1][j]), s[i][j]))
+    constraints.append(Or(Not(bool_vars[n-1]), Not(s[n-2][k-1])))   
+    return And(constraints)
+
+def exactly_k_seq(bool_vars, k, name):
+    return And(at_most_k_seq(bool_vars, k, name), at_least_k_seq(bool_vars, k, name))
