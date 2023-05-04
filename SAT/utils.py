2ba7f6f36e29c8fda80a941865c6d374a59d7be9
Stefano <juststep.2000@gmail.com>
2023-05-04 13:24:12 +0000

    New constraint in testing

--- SAT/utils.py
+++ SAT/utils.py
@@ -11,6 +11,34 @@ def at_most_one_np(bool_vars, name = ""):
 def exactly_one_np(bool_vars, name = ""):
     return And(at_least_one_np(bool_vars), at_most_one_np(bool_vars, name))
 
+###################################################################################################### 1- Bitwise encodings
+
+def toBinary(num, length = None):
+    num_bin = bin(num).split("b")[-1]
+    if length:
+        return "0"*(length - len(num_bin)) + num_bin
+    return num_bin
+    
+def at_least_one_bw(bool_vars):
+    return at_least_one_np(bool_vars)
+
+def at_most_one_bw(bool_vars, name):
+    constraints = []
+    n = len(bool_vars)
+    m = math.ceil(math.log2(n))
+    r = [Bool(f"r_{name}_{i}") for i in range(m)]
+    binaries = [toBinary(i, m) for i in range(n)]
+    for i in range(n):
+        for j in range(m):
+            phi = Not(r[j])
+            if binaries[i][j] == "1":
+                phi = r[j]
+            constraints.append(Or(Not(bool_vars[i]), phi))        
+    return And(constraints)
+
+def exactly_one_bw(bool_vars, name):
+    return And(at_least_one_bw(bool_vars), at_most_one_bw(bool_vars, name)) 
+
 ###################################################################################################### 1 - heule encodings
 
 def at_least_one_seq(bool_vars):
