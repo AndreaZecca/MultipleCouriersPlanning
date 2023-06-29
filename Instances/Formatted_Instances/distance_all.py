3a6c1be49a1471c17ad4109a009a3f7298be0545
AndreaZecca <andreazecca03@gmail.com>
2023-06-29 18:48:52 +0200

    Removed stuffs
    Co-authored-by: Samuele Marro <marrosamuele@gmail.com>

--- Instances/Formatted_Instances/distance_all.py
+++
@@ -1,7 +0,0 @@
-import os
-
-for inst_name in sorted(os.listdir()):
-    if not inst_name.endswith(".dzn"):
-        continue
-    print(inst_name)
-    os.system(f'minizinc distance.mzn ./{inst_name}')
