b38060f7a2c57c574fbdb84a7e9c6b3aac1f079c
AndreaZecca <andreazecca03@gmail.com>
2023-07-04 19:55:25 +0200

    Python interface

---
+++ main.py
@@ -0,0 +1,149 @@
+import json
+import re
+
+import click
+import numpy as np
+
+from SAT.core import run_sat
+from MIP.core import run_mip
+
+import utils
+
+def parse_dat(text):
+    lines = text.split("\n")
+    # regex to get a number from a string
+    num_regex = r"(\d+)"
+    lines = [x for x in lines if x != ""]
+    lines = [x.strip() for x in lines]
+    m = int(re.findall(num_regex, lines[0])[0])
+    n = int(re.findall(num_regex, lines[1])[0])
+    l = [int(x) for x in re.findall(num_regex, lines[2])]
+    s = [int(x) for x in re.findall(num_regex, lines[3])]
+
+    distances = []
+    for i in range(4, 4 + n + 1):
+        distances.append([int(x) for x in re.findall(num_regex, lines[i])])
+
+    return {
+        "m": m,
+        "n": n,
+        "l": l,
+        "s": s,
+        "distances": distances
+    }
+
+def add_additional_info(data):
+    n = data["n"]
+    m = data["m"]
+    l = data["l"]
+    s = data["s"]
+    distances = np.array(data["distances"])
+
+    # count min package per courier
+    each_courier_at_least_one = 0
+    sorted_packages = sorted(s, reverse=True)
+    sorted_load = sorted(l, reverse=True)
+    for i in range(m):
+        if sorted_load[i] < sorted_packages[i]:
+            break
+    else:
+        each_courier_at_least_one = 1
+        
+    time_bound1 = each_courier_at_least_one + n - each_courier_at_least_one * m + 1
+
+    # count max package per courier 
+    max_load = max(l)
+    sorted_packages = sorted(s)
+    max_package_count = 0
+    for package in sorted_packages:
+        max_load -= package
+        if max_load >= 0:
+            max_package_count += 1
+        else:
+            break
+    
+    time_bound2 = max_package_count + 1 # max_package_count + 2 - 1 since 0-indexed
+    
+    time = min(time_bound1, time_bound2)
+
+    # calculate max_dist
+    max_dist_bound1 = np.sum(np.max(distances[:n], axis=1))
+    max_dist_bound2 = np.sum(sorted(distances.flatten(), reverse=True)[:time+1])
+
+    if each_courier_at_least_one == 0:
+        max_dist = max_dist_bound1
+    else:
+        max_dist = np.min([max_dist_bound1, max_dist_bound2])
+
+    min1 = min(distances[n][:n])
+    min2 = min([x[n] for x in distances[:n]])
+
+    
+    if each_courier_at_least_one == 0:
+        min_dist = 0
+    else:
+        min_dist = min(min1, min2)*2
+    
+    # calculate min_solution
+
+    min_solution = np.max([distances[n, j] + distances[j, n] for j in range(n)])
+
+    # calculate min_load and max_load
+    min_load = np.min(s) * each_courier_at_least_one
+    max_load = min(np.max(l), np.sum(sorted(s, reverse=True)[:time]))
+
+    return {
+        **data,
+        "min_dist": min_dist,
+        "max_dist": max_dist,
+        "min_solution": min_solution,
+        "time": time,
+        "at_least_one": each_courier_at_least_one,
+        "min_load": min_load,
+        "max_load": max_load
+    }
+
+def to_mzn(instance):
+    text = ''
+    for k, v in instance.items():
+        if isinstance(v, list):
+            v = np.array(v)
+            if len(v.shape) == 1:        
+                formatted_v = '[' + ', '.join([str(x) for x in v]) + ']'
+            elif len(v.shape) == 2:
+                formatted_v = ''
+                #for i,row in v:
+                #    formatted_v += '[' + ', '.join([str(x) for x in row]) + '],\n'
+
+                v = '[|' + ((' ' * len(k) + ',\n')).join([', '.join([str(y) for y in x]) for x in v]) + '|]'
+        
+        text += f'{k} = {v};\n'
+
+    return text
+
+@click.command()
+@click.argument('config_file', type=click.File('r'))
+def main(config_file):
+    config = json.load(config_file)
+
+    with open(config["instance"], "r") as instance:
+        instance = parse_dat(instance.read())
+    
+    instance = add_additional_info(instance)
+    # print(instance)
+
+    #print(to_mzn(instance))
+    if config['method'] == 'sat':
+        # print(run_sat(instance, config['pb']))
+
+        result = utils.run_with_timeout(run_sat, config['timeout'], instance, config['pb'])
+        
+    elif config['method'] == 'mip':
+        result = run_mip(instance, config['timeout'], config['solver'])
+    else:
+        raise RuntimeError('Unknown method')
+    if result is not None:
+        solution, optimal = result
+        print(solution)
+if __name__ == '__main__':
+    main()
---
+++ utils.py
@@ -0,0 +1,48 @@
+import os
+import signal
+import sys
+import time
+
+import multiprocessing as mp
+
+class DoneException(Exception):
+    pass
+
+def run_with_timeout(f, seconds_before_timeout, *args, **kwargs):
+    def sig_usr1_handler(signum, frame):
+        raise DoneException
+
+    parent_pid = os.getpid()
+
+    queue = mp.Queue()
+
+    new_pid = os.fork()
+    if new_pid == 0:
+        def add_result(result):
+            queue.put(result)
+        final_output = f(add_result, *args, **kwargs)
+        queue.put(final_output)
+        
+        os.kill(parent_pid, signal.SIGUSR1)
+        sys.exit(0)
+    else:
+        old_handler = signal.signal(signal.SIGUSR1, sig_usr1_handler)
+
+        try:
+            time.sleep(seconds_before_timeout)
+            os.kill(new_pid, signal.SIGKILL)
+            completed = False
+        except DoneException:
+            # we receive a KeyboardInterrupt when the child process is done
+            completed = True
+        finally:
+            signal.signal(signal.SIGUSR1, old_handler)
+            result = None
+            while not queue.empty():
+                result = queue.get()
+
+            if result is None:
+                return None
+            else:
+                return result, completed
+
